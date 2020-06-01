6f63eb8d9a320adbd5bee86187431233a73f55c4
Jan Polák <polakjan101@gmail.com>
2020-06-01 23:57:09 +0200

    Remove comments and refactor

--- bulls_cows.py
+++ bulls_cows.py
@@ -3,39 +3,7 @@
 # Description: Project 2 - Part 1 'Bulls and Cows game' in Engeto Online Python Academy
 # Author: Jan Polák
 
-# bulls and cows
-# Tvým úkolem bude vytvořit program, který by simuloval hru Bulls and Cows. Hra funguje následovně:
-#
-#     1 Počítač vygeneruje tajné 4místné číslo. Každá číslice tohoto čísla musí být jiná.
-#     2 Počítač vždy vyzve uživatele, aby hádal toto číslo.
-#     3 Počítač vyhodnotí tip uživatele a vrátí počty shod.
-#     4 Pokud uživatel uhádne správné číslo i správnou pozici, jedná se o "bulls".
-#       Pokud je pozice jiná, ale číslice je správná, jedná se o "cows".
-#
-# Příklad hry s číslem 2017
-#
-
-# Hi there!
-# I've generated a random 4 digit number for you.
-# Let's play a bulls and cows game.
-# Enter a number
-# >>> 1234
-# 0 bulls, 2 cows
-# >>> 6147
-# 1 bull, 1 cow
-# >>> 2417
-# 3 bulls, 0 cows
-# >>> 2017
-# Correct, you've guessed the right number in 4 guesses!
-# That's {amazing, average, not so good, ...}
-#
-# Program toho může umět víc. Můžeš přidat například:
-#
-#     vrácení času, tedy jako dlouho uživateli trvalo číslo uhodnout,
-#     uchovávání statistik počtu odhadů jednotlivých her.
-
 from random import seed, randint
-
 
 def set_rand_num_len():
     while True:
@@ -57,6 +25,7 @@ def set_rand_num_len():
 
 
 def generate_number(rand_num_len):
+
     seed()
     rand_number = []
 
@@ -77,6 +46,7 @@ def check_valid_number(user_in):
 
 
 def user_input(num_len):
+
     while True:
 
         user_num_raw = input("Your number: ")
@@ -112,20 +82,19 @@ def check_conditions(user_num, game_num):
 
 
 def print_welcome(length):
-    print_score("Hello cowboy!")
+    print("Hello, cowboy!")
     print(f"I've generated a random {length} digit number for you.")
     print("Let's play a bulls and cows game.")
 
 
-def print_score(score):
-    # dynamic change of bull | bulls
-    # if score 1 => bull, cow; else bulls, cows
-    pass
+# def print_score(score):
+#     # dynamic change of bull | bulls
+#     # if score 1 => bull, cow; else bulls, cows
+#     pass
 
 
 def game_loop():
     number_length = set_rand_num_len()
-    print("Delka je", number_length)
 
     user_attempt = 1
 
@@ -143,12 +112,10 @@ def game_loop():
         print(f"{user_score['bulls']} bulls, {user_score['cows']} cows")
 
         if user_score["bulls"] == number_length:
-            print(f"You win partner! You need {user_attempt} guesses")
+            print(f"You win, partner! You need {user_attempt} guesses")
             break
         user_attempt += 1
-
 
 if __name__ == '__main__':
     game_loop()
 
-# tic tac toe
