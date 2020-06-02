674a8599623e697ed485f06fe9d6194f18123a28
Jan Polák <polakjan101@gmail.com>
2020-06-02 18:48:27 +0200

    Refactor, add while loop

--- launcher.py
+++ launcher.py
@@ -6,23 +6,31 @@
 from bulls_cows import game_loop as bc_game_loop
 from tic_tac_toe import game_loop as tc_game_loop
 
-print("Choose your game:")
-print("1. Bulls and cows")
-print("2. Tic Tac Toe")
 
 while True:
+    print(16 * "*")
+    print("Choose your game:")
+    print("1. Bulls and cows")
+    print("2. Tic Tac Toe")
+    print("3. Exit")
+    print(16 * "*")
+
     try:
         choice = int(input("Your choice: "))
 
-        if choice in range(1, 3):
-            break
+        if choice in range(1, 4):
+            if choice == 1:
+                bc_game_loop()
+                print("")
+            elif choice == 2:
+                tc_game_loop()
+                print("")
+            else:
+                exit()
         else:
             print("Choose valid number")
             continue
     except ValueError:
         print("This is not number")
 
-if choice == 1:
-    bc_game_loop()
-elif choice == 2:
-    tc_game_loop()
+
